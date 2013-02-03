ba236988e403dece3f24522a734714797fad59e2
Brian Ford <btford@umich.edu>
2013-02-03 00:36:43 -0500

    update definitions

--- app/scripts/scrapeDefinitions.js
+++ app/scripts/scrapeDefinitions.js
@@ -4,13 +4,13 @@ var definitions = {
   amazon: function (document) {
     return {
       name: document.getElementsByClassName('parseasinTitle')[0].innerText.trim(),
-      price: document.getElementsByClassName('priceLarge')[0].innerText.trim()
+      price: document.getElementsByClassName('priceLarge')[0].innerText.match(/\d+\.\d{1,2}/)[0]
     };
   },
   ebay: function (document) {
     return {
       name: document.getElementsByClassName('it-ttl')[0].innerText.trim(),
-      price: document.getElementsByClassName('vi-price')[0].innerText.trim()
+      price: document.getElementsByClassName('vi-price')[0].innerText.match(/\d+\.\d{1,2}/)[0]
     };
   },
 
